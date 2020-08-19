f5c8ea1f09e91b4a7a16ae932273eec6983452bd
ruizj3 <ruizj3@uw.edu>
2020-08-18 19:22:52 -0700

    Update App.js
    trim some unnecessary code for now

--- pill_tracker/src/App.js
+++ pill_tracker/src/App.js
@@ -3,22 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const test = 'Pill and Bill';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{test}</h1>
     </div>
   );
 }

d377d7838e1a1c643823e1decda9aed1ca8349a4
ruizj3 <ruizj3@uw.edu>
2020-09-09 17:46:37 -0700

    Update patientReducer.js
    Removed reducers for separation of duties.

--- pill_tracker/src/reducers/patientReducer.js
+++ pill_tracker/src/reducers/patientReducer.js
@@ -4,33 +4,6 @@ export default function patientReducer(state = {patients: []}, action) {
       return {patients: action.payload}
     case 'ADD_PATIENT':
       return {...state, patients: [...state.patients, action.payload]}
-    case 'ADD_PRESCRIPTION':
-      let patients = state.patients.map(patient => {
-        if (patient.id === action.payload.id) {
-          return action.payload
-        } else {
-          return patient
-        }
-      })
-      return {...state, patients: patients}
-    case 'DELETE_PRESCRIPTION':
-      let patientsTwo = state.patients.map(patient => {
-        if (patient.id === action.payload.id) {
-          return action.payload
-        } else {
-          return patient
-        }
-      })
-      return {...state, patients: patientsTwo}
-    case 'EDIT_PRESCRIPTION':
-      let patientsThree = state.patients.map(patient => {
-        if (patient.id === action.payload.id) {
-          return action.payload
-        } else {
-          return patient
-        }
-      })
-      return {...state, patients: patientsThree}
     default:
       return state
   }

1e018ceaff439ceafc85deafda3a9b81da931918
ruizj3 <ruizj3@uw.edu>
2020-09-09 17:36:04 -0700

    Create TakedosagesContainer.js
    render routes of takedosages

---
+++ pill_tracker/src/containers/TakedosagesContainer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Route, Switch} from 'react-router-dom'
+import {fetchTakedosages} from '../actions/fetchTakedosages'
+import Takedosages from '../components/Takedosages'
+import Takedosage from '../components/Takedosage'
+
+class TakedosagesContainer extends React.Component {
+
+
+  componentDidMount() {
+    Promise.all([
+    this.props.fetchTakedosages()])
+  }
+
+
+  render() {
+      return (
+          <div>
+
+
+            <Switch>
+
+              <Route path='/takedosages/:id' render={(routerProps) => <Takedosage {...routerProps} takedosages={this.props.takedosages}/>}/>
+              <Route path='/takedosages' render={(routerProps) => <Takedosages {...routerProps} takedosages={this.props.takedosages}/>}/>
+            </Switch>
+
+          </div>
+      )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    takedosages: state.takedosageReducer.takedosages
+  }
+}
+
+export default connect(mapStateToProps, {fetchTakedosages})(TakedosagesContainer)

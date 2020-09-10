22f15b9149607c8eaf4213650fe6e0e6ba8bcdd8
ruizj3 <ruizj3@uw.edu>
2020-09-09 19:38:25 -0700

    Update NavBar.js

--- pill_tracker/src/components/NavBar.js
+++ pill_tracker/src/components/NavBar.js
@@ -7,7 +7,13 @@ const NavBar = (props) => {
     <div>
       <Link to='/' style={{paddingRight: '10px'}}>Home  </Link>
       <Link to='/patients' style={{paddingRight: '10px'}}>Patients  </Link>
-      <Link to='/patients/new'> Add Patient</Link>
+      <Link to='/patients/new' style={{paddingRight: '10px'}}> Add Patient</Link>
+      <Link to='/prescriptions' style={{paddingRight: '10px'}}>Prescriptions  </Link>
+      <Link to='/prescriptions/new' style={{paddingRight: '10px'}}>Add Prescription  </Link>
+      <Link to='/takedosages' style={{paddingRight: '10px'}}>Takedosages List  </Link>
+      <Link to='/medications' style={{paddingRight: '10px'}}>Medications  </Link>
+      <Link to='/medications/new' style={{paddingRight: '10px'}}> Add Medication</Link>
+
     </div>
 
   )

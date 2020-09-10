f3531c0faa9aa8d79a2b3316a98563324758ff59
ruizj3 <ruizj3@uw.edu>
2020-09-09 17:31:54 -0700

    Update index.js
    Added combined reducer and bottom app tag

--- pill_tracker/src/index.js
+++ pill_tracker/src/index.js
@@ -4,18 +4,21 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import {BrowserRouter as Router} from 'react-router-dom'
-import patientReducer from './reducers/patientReducer'
+import combineReducers from './reducers/combineReducers'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(patientReducer, composeEnhancers(applyMiddleware(thunk)))
+
+const store = createStore(combineReducers, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <App />
+      (R)Pill and Bill
     </Router>
   </Provider>,
   document.getElementById('root')

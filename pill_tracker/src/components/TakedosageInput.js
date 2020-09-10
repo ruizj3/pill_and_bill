857b2f52695d4b7db3c531483dfdc9fee16a6b15
ruizj3 <ruizj3@uw.edu>
2020-09-09 17:35:18 -0700

    Create TakedosageInput.js
    Basic form that is a confirmation page from selected prescription

---
+++ pill_tracker/src/components/TakedosageInput.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {addTakedosage} from '../actions/addTakedosage'
+import { Redirect } from 'react-router-dom'
+
+
+class TakedosageInput extends React.Component {
+
+  state = {
+    datetaken: this.props.history.location.state.datetaken,
+    prescription_id: this.props.history.location.state.prescription_id
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.addTakedosage(this.state)
+    this.setState({
+      datetaken: '',
+      prescription_id: ''
+    })
+    window.location.reload(false)
+    window.location.replace(`http://localhost:3001/prescriptions`)
+  }
+
+  render() {
+
+    return (
+      <div>
+      Confirm Submission Below:
+        <form onSubmit={this.handleSubmit}>
+          <label>Prescription ID: </label>
+          <input type='text' placeholder='Prescription_ID' value={this.state.prescription_id} name="prescription_id" onChange={this.handleChange}/><br/>
+          <label>Date Taken: </label>
+          <input type='text' placeholder='Date Taken' value={this.state.datetaken} name="datetaken" onChange={this.handleChange}/><br/>
+          <input type="submit"/>
+        </form>
+      </div>
+
+    )
+  }
+}
+
+
+export default connect(null, {addTakedosage})(TakedosageInput)

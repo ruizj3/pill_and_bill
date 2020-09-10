1ed2dafea1db084604481110bf5343c3ab56223c
ruizj3 <ruizj3@uw.edu>
2020-09-09 17:34:39 -0700

    Create Takedosage.js
    Basic render of single Takdosage

---
+++ pill_tracker/src/components/Takedosage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Route, Link} from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
+
+const Takedosage = (props) => {
+
+  const takedosage = props.takedosages.filter(takedosage => takedosage.id == props.match.params.id)[0]
+
+  return (
+    <div>
+      <li>
+        Takedosage_ID: {takedosage ? takedosage.id : null}
+      </li>
+      <li>
+        Prescription Number: <Link to={`/prescriptions/${takedosage ? takedosage.attributes.prescription_id : null}`}>{takedosage ? takedosage.attributes.prescription_id : null}</Link>
+      </li>
+      <li>
+        Date Taken: {takedosage ? takedosage.attributes.datetaken : null}
+      </li>
+
+    </div>
+  )
+}
+
+export default Takedosage

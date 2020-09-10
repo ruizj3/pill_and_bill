6e7972bad3b05f2ac28c2983dc9d016b2cc22f20
ruizj3 <ruizj3@uw.edu>
2020-09-09 19:38:55 -0700

    Update App.js

--- pill_tracker/src/App.js
+++ pill_tracker/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import PatientsContainer from './containers/PatientsContainer'
+import PrescriptionsContainer from './containers/PrescriptionsContainer'
+import TakedosagesContainer from './containers/TakedosagesContainer'
+import MedicationsContainer from './containers/MedicationsContainer'
 
 class App extends React.Component {
 
   render() {
+
     return (
+
       <div className="App">
+
         <PatientsContainer/>
+        <PrescriptionsContainer/>
+        <TakedosagesContainer/>
+        <MedicationsContainer/>
       </div>
     );
   }

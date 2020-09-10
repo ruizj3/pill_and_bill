6db931bcb7dbecfd0aef853c3dea1c2dbf14c1ec
ruizj3 <ruizj3@uw.edu>
2020-09-09 19:38:07 -0700

    Create Medication.js

---
+++ pill_tracker/src/components/Medication.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Redirect} from 'react-router-dom'
+import MedicationEdit from './MedicationEdit'
+import MedicationDelete from './MedicationDelete'
+
+const Medication = (props) => {
+
+  const medication = props.medications.filter(medication => medication.id == props.match.params.id)[0]
+  console.log(medication)
+
+  return (
+    <div>
+      <h2>
+        Medication Name: {medication ? medication.attributes.name : null}
+
+      </h2>
+      {medication ? medication.attributes.prescription.map(prescription =>
+        <li key={prescription.id}>
+          Connected Prescription: {prescription.id}
+        </li>) : null }
+      <h3>Edit Medication</h3>
+      <MedicationEdit medication={medication}/>
+      <h4>Delete Medication</h4>
+      <MedicationDelete medication={medication}/>
+
+    </div>
+  )
+}
+
+export default Medication

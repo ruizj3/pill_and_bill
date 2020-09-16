80ae4a446dd625ab4874e0f615f47510a1c7f795
ruizj3 <ruizj3@uw.edu>
2020-09-16 12:54:17 -0700

    Create Doctors.js
    doctor show for dropdown

---
+++ pill_tracker/src/components/Doctors.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Route, Switch} from 'react-router-dom'
+import {fetchDoctors} from '../actions/fetchDoctors'
+
+class Doctors extends React.Component {
+
+
+  componentDidMount() {
+    Promise.all([
+    this.props.fetchDoctors()])
+  }
+
+
+  render() {
+
+      return (
+        <div>
+        </div>)
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    doctors: state.doctorReducer.doctors,
+  }
+}
+
+export default {
+  ListDoctors: connect(mapStateToProps, {fetchDoctors})(Doctors),
+}

8f112109afd6ab5831288ffd5fc7f2d85cf71474
ruizj3 <ruizj3@uw.edu>
2020-09-16 12:54:56 -0700

    Update combineReducers.js
    added additional reducers for meds and docs

--- pill_tracker/src/reducers/combineReducers.js
+++ pill_tracker/src/reducers/combineReducers.js
@@ -3,10 +3,12 @@ import patientReducer from './patientReducer'
 import prescriptionReducer from './prescriptionReducer'
 import takedosageReducer from './takedosageReducer'
 import medicationReducer from './medicationReducer'
+import doctorReducer from './doctorReducer'
 
 export default combineReducers({
   patientReducer,
   prescriptionReducer,
   takedosageReducer,
-  medicationReducer
+  medicationReducer,
+  doctorReducer
 });

66fb51e648fd1833ff033ef5844cfaa3fecd2a9a
ruizj3 <ruizj3@uw.edu>
2020-09-16 14:54:36 -0700

    Update TakedosagesContainer.js
    Added takedosage input to takedosage container. Necessary for Switch usage in main App component

--- pill_tracker/src/containers/TakedosagesContainer.js
+++ pill_tracker/src/containers/TakedosagesContainer.js
@@ -4,6 +4,7 @@ import {Route, Switch} from 'react-router-dom'
 import {fetchTakedosages} from '../actions/fetchTakedosages'
 import Takedosages from '../components/Takedosages'
 import Takedosage from '../components/Takedosage'
+import TakedosageInput from '../components/TakedosageInput'
 
 class TakedosagesContainer extends React.Component {
 
@@ -20,7 +21,7 @@ class TakedosagesContainer extends React.Component {
 
 
             <Switch>
-
+              <Route path='/takedosages/new' component={TakedosageInput}/>
               <Route path='/takedosages/:id' render={(routerProps) => <Takedosage {...routerProps} takedosages={this.props.takedosages}/>}/>
               <Route path='/takedosages' render={(routerProps) => <Takedosages {...routerProps} takedosages={this.props.takedosages}/>}/>
             </Switch>

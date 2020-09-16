5c1f60f91c48047726e39c5d1e3df5b1e93a5184
ruizj3 <ruizj3@uw.edu>
2020-09-16 12:57:38 -0700

    Update PrescriptionInput.js
    patient and medication dropdown selects

--- pill_tracker/src/components/PrescriptionInput.js
+++ pill_tracker/src/components/PrescriptionInput.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {addPrescription} from '../actions/addPrescription'
 
+
 class PrescriptionInput extends React.Component {
 
   state = {
     dosagestotal: '',
     dosagesper: '',
     dosagesfrequency:'',
-    patient_id:'',
+    patient_id:this.props.history.location.state ? this.props.history.location.state.patient_id : '',
     medication_id:''
   }
 
@@ -29,17 +30,39 @@ class PrescriptionInput extends React.Component {
       medication_id:''
     })
     window.location.reload(false)
-    window.location.replace(`http://localhost:3001/patients`)
+    window.location.replace(`http://localhost:3001/prescriptions`)
   }
 
+  optionChanged = (event) => {
+    this.setState({ patient_id: event.target.value});
+  };
+
+  medicationChanged = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+    this.setState({ medication_id: event.target.value});
+  };
+
   render() {
+
     return (
       <div>
+
         <form onSubmit={this.handleSubmit}>
-          <label>Patient ID: </label>
-          <input type='text' placeholder='Patient_ID' value={this.state.patient_id} name="patient_id" onChange={this.handleChange}/><br/>
-          <label>Medication ID: </label>
-          <input type='text' placeholder='Medication_ID' value={this.state.medication_id} name="medication_id" onChange={this.handleChange}/><br/>
+          <label>Patient Name: </label>
+            <select value={this.state.patient_id} name="patient_id" onChange={this.optionChanged} >
+              {this.props.patients.map(
+                (patient) => <option key={patient.id} value={patient.id}>{patient.attributes.username}</option>)
+              }
+            </select>
+          <br/>
+          <label>Medication Name: </label>
+            <select value={this.state.medication_id} name="medication_id" onChange={this.medicationChanged} >
+              {this.props.medications.map(
+                (medication) => <option key={medication.id} value={medication.id}>{medication.attributes.name}</option>)
+              }
+            </select>
+          <br/>
           <label>Dosages Total: </label>
           <input type='text' placeholder='Dosages Total' value={this.state.dosagestotal} name="dosagestotal" onChange={this.handleChange}/><br/>
           <label>Dosages Per: </label>
@@ -53,5 +76,13 @@ class PrescriptionInput extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    prescriptions: state.prescriptionReducer.prescriptions,
+    patients: state.patientReducer.patients,
+    medications: state.medicationReducer.medications
+  }
+}
 
-export default connect(null, {addPrescription})(PrescriptionInput)
+
+export default connect(mapStateToProps, {addPrescription})(PrescriptionInput)

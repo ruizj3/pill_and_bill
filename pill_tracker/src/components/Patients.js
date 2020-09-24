f330ded50f7bcf429b2f70d7d76d75e7c0636ba3
ruizj3 <ruizj3@uw.edu>
2020-09-23 17:00:23 -0700

    Update Patients.js
    input search field

--- pill_tracker/src/components/Patients.js
+++ pill_tracker/src/components/Patients.js
@@ -2,17 +2,32 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 
-const Patients = props => {
+class Patients extends React.Component {
 
+  state = {
+    username: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {console.log(this.state.username)
+    const searchPatients = this.props.patients.filter(name => name.attributes.username.toLowerCase().includes(this.state.username.toLowerCase()))
   return (
+
     <div>
-      {props.patients.map(patient =>
+      <label>Search Patient Name: </label>
+      <input name="username" onChange={this.handleChange}/>
+
+      {searchPatients.map(patient =>
         <li key={patient.id}>
           <Link to={`/patients/${patient.id}`}>{patient.attributes.username}</Link>
         </li> )}
     </div>
-
   )
+ }
 }
-
 export default Patients

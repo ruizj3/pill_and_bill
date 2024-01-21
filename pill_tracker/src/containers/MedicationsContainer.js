eb73629029138da5f960ae0f9b894375c88414c6
Joseph Ruiz <josephruiz@MacBook-Pro.domain>
2024-01-20 18:58:49 -0800

    Update MedicationsContainer.js
    Revamp for react-redux new paradigm

--- pill_tracker/src/containers/MedicationsContainer.js
+++ pill_tracker/src/containers/MedicationsContainer.js
@@ -1,40 +1,28 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Route, Routes} from 'react-router-dom'
-import {fetchMedications} from '../actions/fetchMedications'
-import Medications from '../components/Medications'
-import Medication from '../components/Medication'
-import MedicationInput from '../components/MedicationInput'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import { fetchMedications } from '../actions/fetchMedications';
+import Medications from '../components/Medications';
+import Medication from '../components/Medication';
+import MedicationInput from '../components/MedicationInput';
 
-class MedicationsContainer extends React.Component {
+function MedicationsContainer() {
+  const dispatch = useDispatch();
+  const medications = useSelector(state => state.medicationsRed.medications);
 
+  useEffect(() => {
+    dispatch(fetchMedications());
+  }, [dispatch]);
 
-  componentDidMount() {
-    Promise.all([
-    this.props.fetchMedications()])
-  }
-
-
-  render() {
-    
-      return (
-          <div>
-
-            <Routes>
-              <Route path='/medications/new' element={MedicationInput}/>
-              <Route path='/medications/:id' render={(routerProps) => <Medication {...routerProps} medications={this.props.medications}/>}/>
-              <Route path='/medications' render={(routerProps) => <Medications {...routerProps} medications={this.props.medications}/>}/>
-            </Routes>
-
-          </div>
-      )
-  }
+  return (
+    <div>
+      <Routes>
+        <Route path='/medications/new' element={<MedicationInput />} />
+        <Route path='/medications/:id' element={<Medication medications={medications} />}/>
+        <Route path='/medications' element={<Medications medications={medications} />}/>
+      </Routes>
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    medications: state.medicationReducer.medications
-  }
-}
-
-export default connect(mapStateToProps, {fetchMedications})(MedicationsContainer)
+export default MedicationsContainer;

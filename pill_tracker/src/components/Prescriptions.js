64e7ad0b6b33f30b668b0880e273aba83c9c3928
Joseph Ruiz <josephruiz@MacBook-Pro.domain>
2024-01-20 16:54:13 -0800

    Update Prescriptions.js
    Updated for new react-redux paradigm

--- pill_tracker/src/components/Prescriptions.js
+++ pill_tracker/src/components/Prescriptions.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Prescriptions = props => {
-  const today = new Date()
+const Prescriptions = ({ prescriptions }) => {
+  const today = new Date();
   return (
     <div>
-      {props.prescriptions.map(prescription =>
-        <li key={prescription.id}>
-          Prescription Number: {prescription.id} -
-          Patient: <Link to={`/patients/${prescription.attributes.patient.id}`}>{prescription.attributes.patient.username}</Link> -
-          Medication: {prescription.attributes.medication.name} -
-          Dosages Per: {prescription.attributes.dosagesper} -
-          Dosages Remaining: {prescription.attributes.dosagestotal} -
-          <Link to={{pathname:`/takedosages/new`, state: { prescription_id: prescription.id, datetaken: today.toJSON().slice(0,10)} }} > Take Dosage</Link>
-        </li> )}
+      <ul>
+        {prescriptions.map(prescription => (
+          <li key={prescription.id}>
+            Prescription Number: {prescription.id} -
+            Patient: <Link to={`/patients/patients/${prescription.attributes.patient.id}`}>
+              {prescription.attributes.patient.username}
+            </Link> -
+            Medication: {prescription.attributes.medication.name} -
+            Dosages Per: {prescription.attributes.dosagesper} -
+            Dosages Remaining: {prescription.attributes.dosagestotal} -
+            <Link to={{
+              pathname: `/takedosages/new`,
+              state: { prescription_id: prescription.id, datetaken: today.toJSON().slice(0, 10) }
+            }}>
+              Take Dosage
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-
-
-  )
+  );
 }
 
-export default Prescriptions
+export default Prescriptions;

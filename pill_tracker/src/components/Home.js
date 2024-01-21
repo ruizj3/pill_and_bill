639891b91292c1f5f0d6601d1699ff2fd732e050
Joseph Ruiz <josephruiz@MacBook-Pro.domain>
2024-01-21 14:08:08 -0800

    Update Home.js
    Short blurb for app

--- pill_tracker/src/components/Home.js
+++ pill_tracker/src/components/Home.js
@@ -1,16 +1,14 @@
-import React from 'react'
-
-
+import React from 'react';
 
 const Home = (props) => {
-
   return (
-      <h1>
-        Welcome to Pill Tracker
-      </h1>
-  )
-
-
-}
+    <div>
+      <h1>Welcome to Pill Tracker</h1>
+      <p>Pill Tracker is your reliable companion in managing your medication schedule.</p>
+      <p>Easily track your prescriptions and dosages all in one place.</p> 
+      <p>Our goal is to help you maintain your health regimen accurately and effortlessly.</p>
+    </div>
+  );
+};
 
 export default Home
